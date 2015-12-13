1a8993816fe0bc860b6d8e569ac69841c5b4499e
Karin Touchie <ktouchie@gmail.com>
2015-12-13 13:01:33 +0100

    Create for loop with if/else statements

---
+++ Exercice7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Exercice7 {
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = (int)(Math.random() * 101);
+        System.out.println("I've chosen a number between 1 and 100. Have a guess:");
+        int guess = sc.nextInt();
+        for (int i = 1; i<8; i++) {
+            if (guess==num) {
+                System.out.println("Congratulations! You correctly guessed the number in " + i " goes.");
+            } else if (guess>num) {
+                System.out.println("The number you guessed is too big. Try again:");
+                guess = sc.nextInt();
+            } else {
+                System.out.println("The number you guessed is too small. Try again:");
+                guess = sc.nextInt();
+            }
+        }
+        if (guess==num) {
+            System.out.println("Congratulations! You correctly guessed the number in 8 goes.");
+        } else {
+            System.out.println("Sorry... You guessed wrong 8 times. The number I chose was " + num);
+        }
+    }
+}
